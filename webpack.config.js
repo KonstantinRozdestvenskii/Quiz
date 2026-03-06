f4fd0cf4494e908feffe2977e7d17c1ae07d411b
Kostya-Rozh <kosta7497@gmail.com>
2026-03-06 22:19:16 +0300

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'production',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "templates", to: "templates" },
+                { from: "styles", to: "styles" },
+                { from: "static/images", to: "images" },
+                { from: "static/fonts", to: "fonts" },
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
